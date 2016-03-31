db32ba55fc56b61b455c6163fa1725ad7f01e433
Gavin Elster <gavin@avvo.com>
2016-03-30 17:35:35 -0700

    Revert move into v1 scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  scope '/v1' do
+  resources :climbs, except: [:new, :edit]
+  resources :users, except: [:new, :edit] do
     resources :climbs, except: [:new, :edit]
-    resources :users, except: [:new, :edit] do
-      resources :climbs, except: [:new, :edit]
-    end
   end
 end
