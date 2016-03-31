5c9697a8eceb18bac62f6967857074a0aa5a39e2
Gavin Elster <gavin@avvo.com>
2016-03-31 16:25:43 -0700

    Rename stair climber to StepUp

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,10 +10,10 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 
-# Although this is not needed for an api-only application, rails4 
-# requires secret_key_base or secret_token to be defined, otherwise an 
+# Although this is not needed for an api-only application, rails4
+# requires secret_key_base or secret_token to be defined, otherwise an
 # error is raised.
 # Using secret_token for rails3 compatibility. Change to secret_key_base
 # to avoid deprecation warning.
 # Can be safely removed in a rails3 api-only application.
-StairClimberApi::Application.config.secret_token = '697990cbf7fd0a244ed3dd0e6acd8e6a2749947f90c293b2f13c84107dc77ca0b2d0643e905a93ca223102505359c4cd67b6cfb9446c5d626c8e9f237059db8c'
+StepUpApi::Application.config.secret_token = '697990cbf7fd0a244ed3dd0e6acd8e6a2749947f90c293b2f13c84107dc77ca0b2d0643e905a93ca223102505359c4cd67b6cfb9446c5d626c8e9f237059db8c'
