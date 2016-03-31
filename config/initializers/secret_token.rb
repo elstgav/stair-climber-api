02c7a0762438c5445018c0a4e9baea52d60632a5
Gavin Elster <gavin@avvo.com>
2016-03-30 17:12:03 -0700

    🚀

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+StairClimberApi::Application.config.secret_token = '697990cbf7fd0a244ed3dd0e6acd8e6a2749947f90c293b2f13c84107dc77ca0b2d0643e905a93ca223102505359c4cd67b6cfb9446c5d626c8e9f237059db8c'
