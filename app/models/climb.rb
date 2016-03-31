f9b6451e918f3adf333252c2cffc2ce543420051
Gavin Elster <gavin@avvo.com>
2016-03-31 09:29:31 -0700

    Add user_flights scope to Climb

--- app/models/climb.rb
+++ app/models/climb.rb
@@ -1,3 +1,11 @@
 class Climb < ActiveRecord::Base
   belongs_to :user
+
+  class << self
+    def user_flights(user_id, date_range = nil)
+      users = where(user_id: user_id)
+      users = users.where(datetime: date_range) if date_range.present?
+      users
+    end
+  end
 end
